14ccb8d6e8b3d10f29202f5b0826075a352d22e6
Ethan J <ethan@yasfu.net>
2025-09-28 05:10:25 -0600

    fix: positions button fix

--- src/main/java/xyz/spaceio/ushop/Listeners.java
+++ src/main/java/xyz/spaceio/ushop/Listeners.java
@@ -7,6 +7,7 @@ import java.util.Map.Entry;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.block.BlockState;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event.Result;
 import org.bukkit.event.EventHandler;
@@ -60,105 +61,108 @@ public class Listeners implements Listener {
 	@SuppressWarnings("deprecation")
 	@EventHandler
 	public void onClick(final InventoryClickEvent e){
-		if(e.getClickedInventory() instanceof PlayerInventory) return;
+		if (e.getClickedInventory() instanceof PlayerInventory) return;
+        if (!(e.getWhoClicked() instanceof Player)) return;
+        if (!plugin.isShopGUI(e.getView())) return;
 
-		if(e.getWhoClicked() instanceof Player) {
-			if(!plugin.isShopGUI(e.getView())) {
-				return;
-			}
-		}else {
-			return;
-		}
-		Bukkit.getScheduler().runTaskLater(plugin, () -> {
+        Bukkit.getScheduler().runTaskLater(plugin, () -> {
 			this.plugin.updateUI(e.getView().getTopInventory());
 		}, 0L);
 
-		if(e.getCurrentItem() != null){
-			if(e.getSlot() >= e.getInventory().getSize() - 9 && e.getSlot() !=  e.getInventory().getSize() - 5) {
-				// skipping deco
-				e.setCancelled(true);
-				return;
-			}
-			
-			if(e.getSlot() == e.getInventory().getSize() - 5 && e.getInventory().getViewers().size() > 0){
-				e.setCancelled(true);
-				e.setResult(Result.DENY);
-				
-				final Player p = (Player) e.getInventory().getViewers().get(0);
-				
-				if(cooldowns.containsKey(p.getName())){
-					if(cooldowns.get(p.getName()) + 2000 > System.currentTimeMillis()){
-						return;
-					}
-				}
-				if(e.getClick() != ClickType.SHIFT_RIGHT && e.getClick() != ClickType.SHIFT_LEFT){
-					//REMOVE SELL ITEM
-					e.getInventory().setItem(e.getSlot(), new ItemStack(Material.AIR));
-					if(e.getCursor() != null){
-						e.getInventory().addItem(e.getCursor());
-						e.setCursor(null);
-					}
-					//SELL
-					double total = plugin.calcWorthOfContent(e.getInventory().getContents());
-					plugin.getEconomy().depositPlayer(p, total);
-					p.sendMessage(plugin.getConfig().getString("message-sold").replace('&', '§').replace("%total%", plugin.getEconomy().format(total)));
-					
-					HashMap<CustomItem, Integer> listOfItems = plugin.getSalableItems(e.getInventory().getContents());
-					List<String> allLines = new ArrayList<>();
-					allLines.add(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("receipt.header")));
-					for (Entry<CustomItem, Integer> entry : listOfItems.entrySet()) {
-						List<String> desc = plugin.getCustomItemDescription(entry.getKey(), entry.getValue(), ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("receipt.format")));
-						for (String str : desc) {
-							String date = new SimpleDateFormat("HH':'mm':'ss").format(new Date());
-							plugin.getLogs().println("[" + date + "] " + p.getName() + " had sold -> " + ChatColor.stripColor(str));
-						}
-						allLines.addAll(desc);
-					}
-					TextComponent receipt = new TextComponent(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("receipt.message")));
-					receipt.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, TextComponent.fromLegacyText(String.join("\n" + ChatColor.RESET, allLines))));
-					p.spigot().sendMessage(receipt);
-					
-					// put unsalable items back to player's inventory
-					for(int i = 0; i < e.getInventory().getSize() - 9; i++){
-						ItemStack is = e.getInventory().getItem(i);
-						if (is == null)
-							continue;
-						if (is.getType().toString().toUpperCase().contains("SHULKER_BOX")) {
-							BlockStateMeta meta = (BlockStateMeta) is.getItemMeta();
-							BlockState state = meta.getBlockState();
-							Inventory container = ((InventoryHolder) state).getInventory();
-							for (int j = 0; j < container.getSize(); j++) {
-								ItemStack shulkerItem = container.getItem(j);
-								if (shulkerItem != null && !shulkerItem.getType().equals(Material.AIR)) {
-									if (shulkerItem.getType() != Material.AIR) {
-										if (plugin.isSalable(shulkerItem)) {
-											container.setItem(j, null);
-										}
-									}
-								}
-							}
-							meta.setBlockState(state);
-							is.setItemMeta(meta);
-							p.getInventory().addItem(is);
-						} else if (is.getType() != Material.AIR && !plugin.isSalable(is)) {
-							p.getInventory().addItem(is);
-						}
+        if (e.getCurrentItem() == null) return;
 
-					}
-					
-					cooldowns.put(p.getName(), System.currentTimeMillis());
-					e.getInventory().clear();
-					//Run later because the inventory bugs if closed immediately.
-					Bukkit.getScheduler().runTaskLater(plugin, new Runnable(){
-						public void run(){
-							p.closeInventory();
-							p.updateInventory();
-						}
-					}, 5L);	
-				}
-			}
-		}
-	}
+        Inventory inv = e.getInventory();
+        FileConfiguration cfg = this.plugin.getConfig();
+
+        List<Integer> positions = cfg.getIntegerList("gui-sellitem.positions");
+        if (positions.isEmpty()) positions.add(inv.getSize() - 5);
+
+        int slot = e.getSlot();
+        boolean isSellBtnItem = positions.contains(slot);
+
+        if (e.getSlot() >= e.getInventory().getSize() - 9 && !isSellBtnItem) {
+            // skipping deco
+            e.setCancelled(true);
+            return;
+        }
+
+        if (isSellBtnItem && !e.getInventory().getViewers().isEmpty()){
+            e.setCancelled(true);
+            e.setResult(Result.DENY);
+
+            final Player p = (Player) e.getInventory().getViewers().get(0);
+
+            if (cooldowns.containsKey(p.getName()) &&
+                    cooldowns.get(p.getName()) + 2000 > System.currentTimeMillis())
+                return;
+
+            if (e.getClick() != ClickType.SHIFT_RIGHT && e.getClick() != ClickType.SHIFT_LEFT){
+                //REMOVE SELL ITEM
+                e.getInventory().setItem(e.getSlot(), new ItemStack(Material.AIR));
+                if(e.getCursor() != null){
+                    e.getInventory().addItem(e.getCursor());
+                    e.setCursor(null);
+                }
+                //SELL
+                double total = plugin.calcWorthOfContent(e.getInventory().getContents());
+                plugin.getEconomy().depositPlayer(p, total);
+                p.sendMessage(plugin.getConfig().getString("message-sold").replace('&', '§').replace("%total%", plugin.getEconomy().format(total)));
+
+                HashMap<CustomItem, Integer> listOfItems = plugin.getSalableItems(e.getInventory().getContents());
+                List<String> allLines = new ArrayList<>();
+                allLines.add(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("receipt.header")));
+                for (Entry<CustomItem, Integer> entry : listOfItems.entrySet()) {
+                    List<String> desc = plugin.getCustomItemDescription(entry.getKey(), entry.getValue(), ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("receipt.format")));
+                    for (String str : desc) {
+                        String date = new SimpleDateFormat("HH':'mm':'ss").format(new Date());
+                        plugin.getLogs().println("[" + date + "] " + p.getName() + " had sold -> " + ChatColor.stripColor(str));
+                    }
+                    allLines.addAll(desc);
+                }
+                TextComponent receipt = new TextComponent(ChatColor.translateAlternateColorCodes('&', plugin.getConfig().getString("receipt.message")));
+                receipt.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, TextComponent.fromLegacyText(String.join("\n" + ChatColor.RESET, allLines))));
+                p.spigot().sendMessage(receipt);
+
+                // put unsalable items back to player's inventory
+                for(int i = 0; i < e.getInventory().getSize() - 9; i++){
+                    ItemStack is = e.getInventory().getItem(i);
+                    if (is == null)
+                        continue;
+                    if (is.getType().toString().toUpperCase().contains("SHULKER_BOX")) {
+                        BlockStateMeta meta = (BlockStateMeta) is.getItemMeta();
+                        BlockState state = meta.getBlockState();
+                        Inventory container = ((InventoryHolder) state).getInventory();
+                        for (int j = 0; j < container.getSize(); j++) {
+                            ItemStack shulkerItem = container.getItem(j);
+                            if (shulkerItem != null && !shulkerItem.getType().equals(Material.AIR)) {
+                                if (shulkerItem.getType() != Material.AIR) {
+                                    if (plugin.isSalable(shulkerItem)) {
+                                        container.setItem(j, null);
+                                    }
+                                }
+                            }
+                        }
+                        meta.setBlockState(state);
+                        is.setItemMeta(meta);
+                        p.getInventory().addItem(is);
+                    } else if (is.getType() != Material.AIR && !plugin.isSalable(is)) {
+                        p.getInventory().addItem(is);
+                    }
+
+                }
+
+                cooldowns.put(p.getName(), System.currentTimeMillis());
+                e.getInventory().clear();
+                //Run later because the inventory bugs if closed immediately.
+                Bukkit.getScheduler().runTaskLater(plugin, new Runnable(){
+                    public void run(){
+                        p.closeInventory();
+                        p.updateInventory();
+                    }
+                }, 5L);
+            }
+        }
+    }
 		
 	
 
